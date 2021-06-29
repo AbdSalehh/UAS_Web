467c1211d626880fddf2a27acf19ad5a0093263d
Saleh-387 <abdshaleh124@webmail.umm.ac.id>
2021-06-29 15:42:25 +0700

    Edit Table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path')->nullable();
-            $table->string('utype')->default('USR')->comment("ADM for Admin and USR for User or Customer");
+            $table->string('utype')->default('ADM')->comment("ADM for Admin and USR for User or Customer");
             $table->timestamps();
         });
     }
